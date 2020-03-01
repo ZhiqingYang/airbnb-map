6f6542c2336545320d8b2d346d97e0b146b0ff94
Vanessa Yang <vanessayang@Vanessas-MacBook-Pro.local>
2020-03-01 11:53:36 -0800

    added map

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const m = {
+    width: 800,
+    height: 600
+}
+
+const svg = d3.select("body").append('svg')
+    .attr('width', m.width)
+    .attr('height', m.height)
+
+
+const g = svg.append('g')
+
+// neighborhoods.json taken from rat map example
+d3.json('nygeo.json').then(function(data) {
+
+    d3.csv('data.csv').then(function(pointData) {
+
+
+        const albersProj = d3.geoAlbers()
+            .scale(190000)
+            .rotate([74, 0]) //negative longitude
+            .center([0, 40.7]) //latitude
+            .translate([m.width/2, m.height/2]);
+
+        const geoPath = d3.geoPath()
+        .projection(albersProj)
+
+        g.selectAll('path')
+        .data(data.features)
+        .enter()
+        .append('path')
+            .attr('fill', '#ccc')
+            .attr('d', geoPath)
+
+        // plots circles on the boston map
+        g.selectAll('.circle')
+            .data(pointData)
+            .enter()
+            .append('circle')
+                .attr('cx', function(d) {
+                    let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                    return scaledPoints[0]
+                })
+                .attr('cy', function(d) {
+                    let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                    return scaledPoints[1]
+                })
+                .attr('r', 2)
+                .attr('fill', '#F08080')
+
+
+    })
+
+})
